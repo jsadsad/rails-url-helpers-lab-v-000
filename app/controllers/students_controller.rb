e6dceb5cd8e2c9dd0bdc18097ae2ef1d4eeffe8c
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-06-28 23:58:11 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,12 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
   end
 
   private
@@ -13,4 +14,6 @@ class StudentsController < ApplicationController
     def set_student
       @student = Student.find(params[:id])
     end
+
+    def active?()
 end
